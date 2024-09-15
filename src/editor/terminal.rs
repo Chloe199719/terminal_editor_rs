83305f6e8d73a85c77861dc30d66107811f05f66
Chloe Pratas <chloevision97@gmail.com>
2024-09-15 14:32:14 +0200

    Refactor code to use terminal_rs instead of test_lsp

--- src/editor/terminal.rs
+++ src/editor/terminal.rs
@@ -1,7 +1,4 @@
-use std::{
-    fmt::Display,
-    io::{stdout, Error, Write},
-};
+use std::io::{stdout, Error, Write};
 
 use crossterm::{
     cursor::{Hide, MoveTo, Show},
@@ -16,18 +13,18 @@ pub struct Size {
     pub height: usize,
     pub width: usize,
 }
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Default)]
 pub struct Position {
-    pub x: usize,
+    pub col: usize,
 
-    pub y: usize,
+    pub row: usize,
 }
 /// Represents the Terminal.
 /// Edge Case for platforms where `usize` < `u16`:
 /// Regardless of the actual size of the Terminal, this representation
 /// only spans over at most `usize::MAX` or `u16::size` rows/columns, whichever is smaller.
 /// Each size returned truncates to min(`usize::MAX`, `u16::MAX`)
-/// And should you attempt to set the cursor out of these bounds, it will also be truncated.
+/// And should you attempt to set the caret out of these bounds, it will also be truncated.
 pub struct Terminal {}
 
 impl Terminal {
@@ -38,7 +35,6 @@ impl Terminal {
     pub fn initialize() -> Result<(), Error> {
         enable_raw_mode()?;
         Self::clear_screen()?;
-        Self::move_cursor_to(Position { x: 0, y: 0 })?;
         Self::execute()?;
         Ok(())
     }
@@ -50,13 +46,13 @@ impl Terminal {
         Self::execute_command(Clear(ClearType::CurrentLine))?;
         Ok(())
     }
-    /// Moves the cursor to the given Position.
+    /// Moves the caret  to the given Position.
     /// # Arguments
-    /// * `Position` - the  `Position`to move the cursor to. Will be truncated to `u16::MAX` if bigger.
-    pub fn move_cursor_to(position: Position) -> Result<(), Error> {
+    /// * `Position` - the  `Position`to move the caret to. Will be truncated to `u16::MAX` if bigger.
+    pub fn move_caret_to(position: Position) -> Result<(), Error> {
         // clippy::as_conversions: See doc above
         #[allow(clippy::as_conversions, clippy::cast_possible_truncation)]
-        Self::execute_command(MoveTo(position.x as u16, position.y as u16))?;
+        Self::execute_command(MoveTo(position.col as u16, position.row as u16))?;
         Ok(())
     }
     /// Returns the current size of this Terminal.
@@ -72,16 +68,16 @@ impl Terminal {
         let width = width_u16 as usize;
         Ok(Size { height, width })
     }
-    pub fn print<T: Display>(string: T) -> Result<(), Error> {
+    pub fn print(string: &str) -> Result<(), Error> {
         Self::execute_command(Print(string))?;
         Ok(())
     }
-    pub fn hide_cursor() -> Result<(), Error> {
+    pub fn hide_caret() -> Result<(), Error> {
         Self::execute_command(Hide)?;
 
         Ok(())
     }
-    pub fn show_cursor() -> Result<(), Error> {
+    pub fn show_caret() -> Result<(), Error> {
         queue!(stdout(), Show)?;
 
         Ok(())
